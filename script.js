89fbf5ef55aeeeea269cba3a0b73825bcc0ee6ab
gomenjukmarija <gomenjukmarija@gmail.com>
2017-11-16 09:52:48 +0200

    Module 4 Coding Assignment

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
+
+(function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+    for (var name in names) {
+
+        var firstLetter = names[name].charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[name]);
+        } else {
+            helloSpeaker.speak(names[name]);
+        }
+
+    }
+})();
